45703a1e9dcab58c7283184aa8382d9019550cf9
yuchiro22 <t.yuyu.alies@gmail.com>
2021-05-12 08:18:45 +0900

    initial commit

---
+++ point-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  point-app
+//
+//  Created by Yuichiro Tsuji on 2021/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ point-app/point_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  point_appApp.swift
+//  point-app
+//
+//  Created by Yuichiro Tsuji on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct point_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
